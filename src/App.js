f7899d47a2d97951fe7cd7610e5e6bc0943887b9
LPTruong <48116041+LPTruong@users.noreply.github.com>
2020-12-21 19:47:40 +0700

    add components

--- src/App.js
+++ src/App.js
@@ -9,35 +9,50 @@ import Footer from "./containers/LandingPage/Footer";
 import "antd/dist/antd.css";
 import Register from "./containers/Register/Register";
 import ForgotPassword from "./containers/Forgot_Password/Forgot_Passsword";
-
+import Logged from "./Logged";
 const App = () => {
   return (
     <BrowserRouter>
-      <Header index={1} />
       <Switch>
         <Route exact path="/">
+          <Header index={1} />
           <Home />
+          <Footer />
         </Route>
         <Route path="/aboutus">
+          <Header index={1} />
           <AboutUs />
+          <Footer />
         </Route>
         <Route path="/blog">
+          <Header index={1} />
           <Blog />
+          <Footer />
         </Route>
         <Route path="/contactus">
+          <Header index={1} />
           <ContactUs />
+          <Footer />
         </Route>
         <Route path="/login">
+          <Header index={1} />
           <Login />
+          <Footer />
         </Route>
         <Route path="/register">
+          <Header index={1} />
           <Register />
+          <Footer />
         </Route>
         <Route path="/forgot_password">
+          <Header index={1} />
           <ForgotPassword />
+          <Footer />
+        </Route>
+        <Route path="/Logged">
+          <Logged />
         </Route>
       </Switch>
-      <Footer />
     </BrowserRouter>
   );
 };
--- src/Logged.js
+++ src/Logged.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'antd/dist/antd.css';
-import './index.css';
-import { Layout, Menu } from 'antd';
-import { Image } from 'antd';
-import corgi1 from "./assets/corgi1.jpg"
-import corgi2 from "./assets/corgi2.jpg"
-import corgi3 from "./assets/corgi3.jpg"
-import corgi4 from "./assets/corgi4.jpg"
-import corgi5 from "./assets/corgi5.jpg"
-import corgi6 from "./assets/corgi6.jpg"
-import { Input } from 'antd';
-import { AudioOutlined } from '@ant-design/icons';
+import React from "react";
+import ReactDOM from "react-dom";
+import "antd/dist/antd.css";
+import "./index.js";
+import { Layout, Menu } from "antd";
+import { Image } from "antd";
+import corgi1 from "./assets/corgi1.jpg";
+import corgi2 from "./assets/corgi2.jpg";
+import corgi3 from "./assets/corgi3.jpg";
+import corgi4 from "./assets/corgi4.jpg";
+import corgi5 from "./assets/corgi5.jpg";
+import corgi6 from "./assets/corgi6.jpg";
+import { Input } from "antd";
+import { AudioOutlined } from "@ant-design/icons";
 import {
   AppstoreOutlined,
   BarChartOutlined,
@@ -21,94 +21,83 @@ import {
   UserOutlined,
   UploadOutlined,
   VideoCameraOutlined,
-} from '@ant-design/icons';
+} from "@ant-design/icons";
 const suffix = (
   <AudioOutlined
     style={{
       fontSize: 16,
-      color: '#1890ff',
+      color: "#1890ff",
     }}
   />
 );
 const { Search } = Input;
-const onSearch = value => console.log(value);
+const onSearch = (value) => console.log(value);
 const { Header, Content, Footer, Sider } = Layout;
-const App = () => {
+const Logged = () => {
   return (
-    <Layout>  
-<Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
-      <div className="logo" />
-      <Menu theme="dark" mode="horizontal" >
-      <Search
-      placeholder="input search text"
-      allowClear
-      enterButton="Search"
-      size="large"
-      onSearch={onSearch}
-    />
-        <Menu.Item key="1">nav 1</Menu.Item>
-        <Menu.Item key="2">nav 2</Menu.Item>
-        <Menu.Item key="3">nav 3</Menu.Item>
-      </Menu>
-    </Header>
-    <Sider
-      style={{
-        overflow: 'auto',
-        height: '100vh',
-        position: 'fixed',  
-        left: 0,
-      }}
-    >
-      <div className="logo" />
-      <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}> 
-        <Menu.Item key="1" icon={<UploadOutlined />}>
-          Picture
-        </Menu.Item>
-        <Menu.Item key="2" icon={<VideoCameraOutlined />}>
-          Album
-        </Menu.Item>
-        <Menu.Item key="3" icon={<CloudOutlined />}>
-          Sharing
-        </Menu.Item>
-        <Menu.Item key="4" icon={<ShopOutlined />}>
-          Bin
-        </Menu.Item>
-      </Menu>
-    </Sider>
-    <Layout className="site-layout" style={{ marginLeft: 200 }}>
-      <Header className="site-layout-background" style={{ padding: 0 }} />
-      <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
-        <div className="site-layout-background" style={{ padding: 24, textAlign: 'center' }}>
-        </div>
-        <div className="images-grid-2"> 
+    <Layout>
+      <Header style={{ position: "fixed", zIndex: 1, width: "100%" }}>
+        <div className="logo" />
+        <Menu theme="dark" mode="horizontal">
+          <Search
+            placeholder="input search text"
+            allowClear
+            enterButton="Search"
+            size="large"
+            onSearch={onSearch}
+          />
+          <Menu.Item key="1">nav 1</Menu.Item>
+          <Menu.Item key="2">nav 2</Menu.Item>
+          <Menu.Item key="3">nav 3</Menu.Item>
+        </Menu>
+      </Header>
+      <Sider
+        style={{
+          overflow: "auto",
+          height: "100vh",
+          position: "fixed",
+          left: 0,
+        }}
+      >
+        <div className="logo" />
+        <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
+          <Menu.Item key="1" icon={<UploadOutlined />}>
+            Picture
+          </Menu.Item>
+          <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+            Album
+          </Menu.Item>
+          <Menu.Item key="3" icon={<CloudOutlined />}>
+            Sharing
+          </Menu.Item>
+          <Menu.Item key="4" icon={<ShopOutlined />}>
+            Bin
+          </Menu.Item>
+        </Menu>
+      </Sider>
+      <Layout className="site-layout" style={{ marginLeft: 200 }}>
+        <Header className="site-layout-background" style={{ padding: 0 }} />
+        <Content style={{ margin: "24px 16px 0", overflow: "initial" }}>
+          <div
+            className="site-layout-background"
+            style={{ padding: 24, textAlign: "center" }}
+          ></div>
+          <div className="images-grid-2">
             <div>
-            <Image
-            width={200}
-            src = {corgi1}
-            />
-            <Image
-            width={200}
-            src = {corgi2}
-            />
+              <Image width={200} src={corgi1} />
+              <Image width={200} src={corgi2} />
             </div>
             <div>
-            <Image
-            width={200}
-            src = {corgi3}
-            />
-            <Image
-            width={200}
-            src = {corgi4}
-            />
+              <Image width={200} src={corgi3} />
+              <Image width={200} src={corgi4} />
             </div>
-        </div>
-    
-  );
-      </Content>
-      <Footer style={{ textAlign: 'center' }}></Footer>
+          </div>
+          );
+        </Content>
+        <Footer style={{ textAlign: "center" }}></Footer>
+      </Layout>
     </Layout>
-  </Layout>
-  )
-}
+  );
+};
 
-export default App;
+export default Logged;
